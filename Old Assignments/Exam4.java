01a14c20d56a3e2d9207b41e935e6a36cc789bff
jonathanbalewicz <njaohnt@gmail.com>
2018-12-29 17:33:44 -0600

    added assignments

--- Old Assignments/Exam4.java
+++
@@ -1,9 +0,0 @@
-public double avgGPA(ArrayList<Student> classList) {
-  double sum = 0.00;
-  double result;
-  for (int i = 0; i < classList.size(); i++) {
-    sum += classList.get(i).getGPA();
-  }
-  result = sum/((double)classList.size());
-  return result;
-}
